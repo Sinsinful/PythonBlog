d1fcc424c1b0cb7295512d2470f52facd0f83d64
Kieran Smyth <sin_1210@hotmail.com>
2022-07-21 16:29:36 +0100

    Initial commit

---
+++ theblog/urls.py
@@ -0,0 +1,15 @@
+from unicodedata import category
+from django.urls import path
+
+from theblog.models import Category
+from .views import AddPostView, HomeView, ArticleDetailView, UpdatePostView, UpdatePostView, DeletePostView, AddCategoryView, CategoryView
+
+urlpatterns = [
+    path('', HomeView.as_view(), name ="home"),
+    path('article/<int:pk>', ArticleDetailView.as_view(), name='article-detail'),
+    path('add_post/', AddPostView.as_view(), name='add_post'),
+    path('add_category/', AddCategoryView.as_view(), name='add_category'),
+    path('article/edit/<int:pk>', UpdatePostView.as_view(), name='update_post'),
+    path('article/<int:pk>/remove', DeletePostView.as_view(), name='delete_post'),
+    path('category/<str:cats>/', CategoryView, name='category'),
+]
